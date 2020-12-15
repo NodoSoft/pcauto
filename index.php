f6439817feb37016241c81005d4f1d5b53d6dedc
NodoSoft <73980645+NodoSoft@users.noreply.github.com>
2020-12-15 14:26:20 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 
     // Header
     // header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Origin: http://localhost:4200');
-    header('Access-Control-Allow-Origin: '.$dominioPermitido);
+    header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
     header('Access-Control-Allow-Headers: Content-Type');
